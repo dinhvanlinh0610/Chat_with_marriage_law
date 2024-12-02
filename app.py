191b803343d77fb9482e2fd2a229beb498abc555
Đinh Văn Linh <2051063562@e.tlu.edu.vn>
2024-12-02 17:43:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,23 @@
 from fastapi import FastAPI
 
 # Import Utils
-from utils import process_data, split_data, save_data
+from utils import process_data, split_data, save_data, init_llm
 
 # Import Embedding Model
 from embedding import HuggingFaceEmbedding
 
-
 ### Initialize FastAPI
 app = FastAPI()
 
+# Initialize chat session
+session_state = {
+    "embedding_model": None,
+    "llm": None,
+    "chroma": None,
+    "collection_name": None,
+    "history_chat": []
+
+}
 
 ### Endpoint ###
 
@@ -24,7 +32,24 @@ def read_root():
     str: A message to introduce chatbot.
 
     """
+
     return {"message": "Welcome to the chatbot! Please enter a message to start a conversation."}
+
+@app.get("/initialize/")
+def initialize(model_name: str, api_key: str, type_llm: str):
+    """
+    This is a function to initialize the model.
+
+    Returns:
+    str: A message to show that the model is initialized.
+
+    """
+    try:
+        session_state["llm"] = init_llm(model_name, api_key, type_llm)
+    except Exception as e:
+        # Log error
+        return {"error": str(e)}
+    return "Model is initialized!"
 
 # endpoint nhận vào data_path và trả về danh sách các câu trong file pdf
 @app.get("/upload_file/")
@@ -40,8 +65,31 @@ def upload_file(data_path: str):
 
     """
     embedding_model = HuggingFaceEmbedding("keepitreal/vietnamese-sbert")
+    session_state["embedding_model"] = embedding_model
+
     documents = process_data(data_path)
     docs = split_data(documents, embedding_model.embedding)
     chroma = save_data(collection_name="test", embedding_model=embedding_model.embedding, documents=docs)
+    session_state["chroma"] = chroma
     return "success"
+
+@app.get("/setup/")
+def setup(type_search: str):
+    """
+    This is a function to setup the chatbot.
+
+    Returns:
+    str: A message to show that the chatbot is setup.
+
+    """
+    if type_search == "semantic":
+        return "Setup semantic search!"
+
+@app.post("/chat/")
+def chatbot(message: str):
+
+    llm = session_state["llm"]
+    response = llm.generate_content(message)
+    return {"response": response}
+
 
