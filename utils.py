267603854195b0e646f05fe843bbbd9a3239f257
Đinh Văn Linh <2051063562@e.tlu.edu.vn>
2024-12-02 17:43:42 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from loader import PDFLoader
 from chunker import SemanticChunk
 from vector_store import ChromaDB
+from llms import OnlineLLM
+from llms.local_llms import run_ollama_container, run_ollama_model
 # Hàm xử lý dữ liệu nhận được từ client 
 def process_data(data):
     """
@@ -45,4 +47,59 @@ def save_data(collection_name="test", embedding_model=None, path="./db/chroma_db
     chroma = vector_store.add(documents=documents)
     return chroma
 
+def init_local_llms():
+    print("Run ollama container")
+    run_ollama_container()
 
+    print("select model")
+
+    # cho chọn model bằng list(OLLAMA_MODEL_OPTIONS.keys())
+    selected_model = "llama3.2"
+
+    print("Run model")
+    localLLMs = run_ollama_model(selected_model)
+
+    return localLLMs
+def init_llm(model_name = "llama3-8b-8192", api_key = None, type_llm = "online"):
+    """
+    This function initializes the language model.
+
+    Args:
+        model_name (str): The name of the model.
+        api_key (str): The API key.
+        type_llm (str): The type of language model.
+
+    Returns:
+        object: The language model.
+    """
+    if type_llm == "online":
+        llm = OnlineLLM(model_name, api_key)
+    else:
+        llm = init_local_llms()
+    return llm
+
+def RAG(llm, chroma, query, k= None, direct = None, type_search = "semantic"):
+
+    """
+    This function takes in a query and returns a response using the RAG model.
+
+    Args:
+        llm (object): The language model.
+        chroma (object): The vector store.
+        query (str): The query to be searched.
+        k (int): The number of documents to be returned.
+        direct (str): The source of the document.
+
+    Returns:
+        list: List of documents.
+
+    """
+    if direct:
+        if type_search == "vector":
+            docs = chroma.query_directly(query, k, direct)
+    
+
+
+        return chroma.query_directly(query, k, direct)
+    else:
+        return llm.query(query, chroma, k)
